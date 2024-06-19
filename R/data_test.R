a44ae75899aa63172fe07c2bd179bc3d2636e04c
Shu Fai Cheung <shufai.cheung@gmail.com>
2024-06-19 20:26:06 +0800

    Add a test dataset

--- R/data_test.R
+++ R/data_test.R
@@ -31,3 +31,29 @@
 #'
 #'
 "data_test_medmod"
+
+#' @title Test Dataset with Moderator
+#' and Categorical Variables
+#'
+#' @description This dataset has one
+#' predictor, one moderator, one
+#' control variable, one dependent
+#' variable, and a categorical variable.
+#'
+#' @format A data frame with 500 rows
+#' and five variables:
+#' \describe{
+#'   \item{dv}{Dependent variable, continuous}
+#'   \item{iv}{Independent variable, continuous}
+#'   \item{mod}{Moderator, continuous}
+#'   \item{cov1}{Control variable, continuous}
+#'   \item{cat1}{String variable with these values: "gp1", "gp2", and "gp3"}
+#' }
+#'
+#' @examples
+#'
+#' lm_out <- lm(dv ~ iv * mod + cov1 + cat1, data_test_mod_cat)
+#' summary(lm_out)
+#'
+#'
+"data_test_mod_cat"

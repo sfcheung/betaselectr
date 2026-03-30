152edae02084c893b14ed283e491333eef60e636
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-03-30 22:29:58 +0800

    Update rebuild_vignettes.R

--- rebuild_vignettes.R
+++ rebuild_vignettes.R
@@ -3,9 +3,9 @@
 base_dir <- getwd()
 
 setwd("vignettes/")
-knitr::knit("betaselectr_lav.Rmd.original", output = "betaselectr_lav.Rmd")
-knitr::knit("betaselectr_lm.Rmd.original", output = "betaselectr_lm.Rmd")
-knitr::knit("betaselectr_glm.Rmd.original", output = "betaselectr_glm.Rmd")
+knitr::knit("betaselectr_lav.Rmd.original", output = "betaselectr_lav.Rmd", envir = new.env())
+knitr::knit("betaselectr_lm.Rmd.original", output = "betaselectr_lm.Rmd", envir = new.env())
+knitr::knit("betaselectr_glm.Rmd.original", output = "betaselectr_glm.Rmd", envir = new.env())
 setwd(base_dir)
 
 # For articles
